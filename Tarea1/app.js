b4a196baea0421847079a612cfd0e955baf8b396
MimiCrucesH <mimicruces1902@gmail.com>
2024-08-22 17:02:35 -0600

    Tarea: Calculo de IMC

---
+++ Tarea1/app.js
@@ -0,0 +1,8 @@
+let nombre = prompt("Ingresa tu nombre: ");
+let edad = prompt("Ingresa tu edad: ");
+let peso = prompt("Ingresa tu peso (kg): ");
+let estatura = prompt("Ingresa tu estatura (m):");
+
+let IMC = parseFloat(peso)/(parseFloat(estatura)*parseFloat(estatura));
+console.log(nombre);
+console.log("Tu IMC es: "+ IMC);
