b504bcd9e0d18d1f791f14e57a8891a8f7837973
MimiCrucesH <mimicruces1902@gmail.com>
2024-08-31 05:55:01 -0600

    Agregar en promt el mensaje de salida

--- Tarea1/app.js
+++ Tarea1/app.js
@@ -6,3 +6,5 @@ let estatura = prompt("Ingresa tu estatura (m):");
 let IMC = parseFloat(peso)/(parseFloat(estatura)*parseFloat(estatura));
 console.log(nombre);
 console.log("Tu IMC es: "+ IMC);
+
+prompt(nombre + " tu IMC es: "+ IMC);
